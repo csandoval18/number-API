30ddcb58cc825837e56c64b5a95cb27efcb76daf
Christian AS <alvitesscf18@uww.edu>
2022-06-05 21:05:07 -0500

    first commit

---
+++ src/useFetch.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export const useFetch = (url) => {
+	const [state, setState] = useState({ data: null });
+	useEffect(() => {
+		setState((state) => ({ data: state.data }));
+		fetch(url)
+			.then((x) => x.text())
+			.then((y) => {
+				setState({ data: y, loading: false });
+			});
+	}, []);
+	return state;
+};
